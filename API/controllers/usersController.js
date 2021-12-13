a5c4257b44a120d773a1a57922be5ed3c4d82285
brunojosue <bruno7josue@gmail.com>
2021-12-13 11:55:09 +0000

    update_13.12.2021

--- API/controllers/usersController.js
+++ API/controllers/usersController.js
@@ -8,5 +8,39 @@ router.get('/', (req, res) => {
     res.status(200).json(users);
 });
 
+router.get('/:id', async (req, res) => {
+    const user = await User.findByPk(req.params.id);
+    console.log(user);
+    res.status(200).json(user);
+});
+
+router.post('/', (req, res) => {
+    const { firstName } = req.body;
+    const newUser = User.create({ firstName });
+
+    res.status(200).json("User created successfully!!");
+});
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where:{
+            id: req.params.id,
+        },
+    });
+
+    res.status(200).json("User deleted successfully!!")
+});
+
+router.put('/:id', (req, res) => {
+    const { firstName } = req.body;
+
+    User.update({ firstName }),
+    {
+        where:{
+            id: req.params.id
+        },
+    };
+});
+
 module.exports = router;
 
