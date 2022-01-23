d98cf5cf0ee9f283a56f161bcf4850cefbd74529
Sérgio Félix <sergio98@outlook.pt>
2022-01-23 21:56:29 +0000

    Merge branch 'main' of https://github.com/brunojosue/LP

--- src/middlewares/notfound.js
+++
@@ -1,8 +0,0 @@
-const notFound = (req, res, next) => {
-	res.status(404).send({
-		status: 404,
-		message: "Oops! Sorry, we couldn't found you're looking for.",
-	});
-};
-
-module.exports = notFound;
