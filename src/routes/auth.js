3022500058e3ff1b1af9ec8bc0d97b3ccf7a1b9b
Sérgio Félix <danieldosfarias@gmail.com>
2022-01-24 11:22:50 +0000

    dasdasdas
    dasdas

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -12,6 +12,7 @@ function AuthRouter() {
 	router.use(express.json({ limit: '100mb' }));
 	router.use(express.urlencoded({ limit: '100mb', extended: true }));
 
+<<<<<<< HEAD
 	router.route('/sign-up').post(
 		body('name').trim().notEmpty().withMessage('Name cannot be empty!'),
 		body('email').trim().notEmpty().withMessage('Email cannot be empty!').bail().normalizeEmail().isEmail().withMessage('You must provide a valid email address!'),
@@ -41,9 +42,29 @@ function AuthRouter() {
 						return res.cookie('token', token, { maxAge: 5400000, expires: new Date(Date.now() + 5400000), httpOnly: true }).status(200).send({ status: 200, auth: true, path: req.originalUrl, message: 'Successfully signed up!', data: token });
 					});
 				});
+=======
+	router.route('/sign-up').post(async (req, res, next) => {
+		pool.getConnection(async (error, connection) => {
+			if (error) console.error(error);
+            const data = {
+				name: req.body.name,
+				email: req.body.email,
+				password: await bcrypt.hash(req.body.password, 10),
+				phone_number: req.body.phone,
+			};
+			const query = 'INSERT INTO users (name,email,password,phone_number) VALUES (?,?,?,?)';
+			connection.query(query, Object.values(data), async (error, results) => {
+				connection.release();
+				if (error) console.error(error);
+				if (results && !results.length) res.status(200).send({ status: 400, auth: false, message: 'Unsuccessfully signed up!', data: [] });
+                const token = await jwt.sign({ email: data.email }, 'FLG-LP@2122', { algorithm: 'HS256' }, { expiresIn: 1800000 })
+				res.cookie('token', token, {maxAge: 1800000, expires: new Date(Date.now() + 1800000), httpOnly: true})
+                .status(200).send({ status: 200, auth: true, message: 'Successfully signed up!', data: results });
+>>>>>>> parent of 766633f (35)
 			});
 		});
 
+<<<<<<< HEAD
 	router.route('/sign-in').post(
 		body('email').trim().notEmpty().withMessage('Email cannot be empty!').bail().normalizeEmail().isEmail().withMessage('You must provide a valid email address!'),
 		body('password').trim().notEmpty().withMessage('Password cannot be empty!').bail().isLength({ min: 6, max: 18 }).withMessage('Password must be at least 8 characters and max 18 characters long!'),
@@ -73,6 +94,48 @@ function AuthRouter() {
 
 	router.route('/signed').get(verifyJWT, async (req, res, next) => {
 		return res.status(200).send({ status: 200, auth: true, message: 'User is currently authenticated!', data: [] })
+=======
+	router.route('/sign-in').post(async (req, res, next) => {
+		pool.getConnection(async (error, connection) => {
+			if (error) console.error(error);
+			const data = {
+				email: req.body.email,
+				password: req.body.password,
+			};
+			const query = 'SELECT * FROM users WHERE email = ?';
+			connection.query(query, Object.values(data), async (error, results) => {
+				connection.release();
+				if (error) console.error(error);
+				if (results && !results.length) res.status(200).send({ status: 400, auth: false, message: 'Unsuccessfully signed in!', data: [] });
+                const match = await bcrypt.compare(data.password, results.password);
+                console.log(match);
+                if(!match) res.status(200).send({ status: 400, auth: false, message: 'Email or password are wrong!', data: [] });
+                const token = await jwt.sign({ email: data.email }, 'FLG-LP@2122', { algorithm: 'HS256' }, { expiresIn: 1800000 })
+				res.cookie('token', token, {maxAge: 1800000, expires: new Date(Date.now() + 1800000), httpOnly: true})
+                .status(200).send({ status: 200, auth: true, message: 'Successfully signed in!', data: results });
+			});
+		});
+	});
+
+	router.route('/sign-out').get(async (req, res, next) => {
+        res.clearCookie('token').status(200).send({ status: 200, auth: false, message: ''})
+	});
+
+	router.route('/signed').get(async (req, res, next) => {
+		pool.getConnection((error, connection) => {
+			if (error) console.error(error);
+			const data = {
+				idusers: req.params.id,
+			};
+			const query = 'DELETE FROM users WHERE idusers = ?';
+			connection.query(query, Object.values(data), (error, results) => {
+				connection.release();
+				if (error) console.error(error);
+				if (!results) res.status(200).send({ status: 400, message: 'User unsuccessfully deleted!', data: [] });
+				res.status(200).send({ status: 200, message: 'User successfully deleted!', data: results });
+			});
+		});
+>>>>>>> parent of 766633f (35)
 	});
 
 	return router;
